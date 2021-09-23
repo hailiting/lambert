44b887f645849a550b6df055d4fad299bea41a75
hai.liting <hailiting@example.com>
2021-09-23 19:07:21 +0800

    页面好

--- src/components/header/headerWidget.tsx
+++ src/components/header/headerWidget.tsx
@@ -7,14 +7,18 @@ function HeaderWidget() {
       <div className="headerWidget">
         <h1>
           <a href="/" title="0xLambert">
-            0xLambert
+            <img src={require("./img/logo.png")} alt="logo" />
           </a>
         </h1>
         <h2>Unique SOULLY</h2>
         <h3>coming soon</h3>
         <div className="fr">
-          <img src={require("./img/discord.png")} alt="discord" />
-          <img src={require("./img/twitter.png")} alt="twitter" />
+          <a href="https://discord.gg/Dmn7DhDyEZ" title="discord.gg">
+            <img src={require("./img/discord.png")} alt="discord" />
+          </a>
+          <a href="https://twitter.com/SoullyWorld" title="twitter.com">
+            <img src={require("./img/twitter.png")} alt="twitter" />
+          </a>
         </div>
       </div>
     </div>
