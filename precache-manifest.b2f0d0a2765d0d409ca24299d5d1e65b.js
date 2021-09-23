a387f462c6e6c67967b657015e57dc1e89f23f0a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-09-23 11:44:29 +0000

    Deploy hailiting/lambert to github.com/hailiting/lambert.git:gh-pages

---
+++ precache-manifest.b2f0d0a2765d0d409ca24299d5d1e65b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d427714d27dc5d87995ada6060ef89a8",
+    "url": "/lambert/index.html"
+  },
+  {
+    "revision": "e716e97949381bb7cae9",
+    "url": "/lambert/static/css/3.8a468b97.chunk.css"
+  },
+  {
+    "revision": "3ab190f348cb964803d5",
+    "url": "/lambert/static/css/main.9223e807.chunk.css"
+  },
+  {
+    "revision": "04f9903b14f807f1e4ca",
+    "url": "/lambert/static/js/2.602a113e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/lambert/static/js/2.602a113e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e716e97949381bb7cae9",
+    "url": "/lambert/static/js/3.6e948124.chunk.js"
+  },
+  {
+    "revision": "3ab190f348cb964803d5",
+    "url": "/lambert/static/js/main.2fa8b3e1.chunk.js"
+  },
+  {
+    "revision": "08a69c9bc8bec205ccf6",
+    "url": "/lambert/static/js/runtime-main.5e0aed66.js"
+  },
+  {
+    "revision": "853850ef997e982a03c77ca2194c842b",
+    "url": "/lambert/static/media/bg.853850ef.png"
+  },
+  {
+    "revision": "d5213044cfad1e4f4b5d1f3138752a80",
+    "url": "/lambert/static/media/erasbd.d5213044.ttf"
+  },
+  {
+    "revision": "a3a32c4d6e4abdfd3c6dd289e32fac6c",
+    "url": "/lambert/static/media/pc01.a3a32c4d.png"
+  },
+  {
+    "revision": "5f8bc9a7bd899209c1b71770f0dc09e1",
+    "url": "/lambert/static/media/pc02.5f8bc9a7.png"
+  },
+  {
+    "revision": "524e5004820ef8693da21a256f15ddfc",
+    "url": "/lambert/static/media/pc03.524e5004.png"
+  },
+  {
+    "revision": "35046cab6b043adccf0f9ab5a82e3146",
+    "url": "/lambert/static/media/pc04.35046cab.png"
+  },
+  {
+    "revision": "1dd6ac3aa738969f477fc56e4dccaa06",
+    "url": "/lambert/static/media/pc05.1dd6ac3a.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/lambert/precache-manifest.b2f0d0a2765d0d409ca24299d5d1e65b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lambert/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
